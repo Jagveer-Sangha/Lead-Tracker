22d58289ed2110a1311a1eca05e68036ef0336be
Jagveer Sangha <jagveersangha@gmail.com>
2021-06-30 10:59:12 -0400

    Added Tab button event listener, and refactored code

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
 
 let myLeads = []
 
-// Can't push strings into an list --> `["www.awesomelead.com"]`
-// Storing arrays in localstorage
-
-
-
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
 // Get the leads from the localStorage
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
 const deleteBtn = document.getElementById("delete-btn")
+
+const tabBtn = document.getElementById("tab-btn")
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 }
 
-deleteBtn.addEventListener("dblclick", function () {
-    localStorage.clear()
-    myLeads = []
-    renderLeads()
+tabBtn.addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    });
 })
+
+// Can take in any array and render it out
+function render(leads) {
+    let listItems = ""
+
+    for (let i = 0; i < leads.length; i++) {
+        // Utilized Template strings to create more user friendly code
+
+        // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+
+        listItems +=
+            `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    // Render the listItems inside the unordered list
+    ulEl.innerHTML = listItems
+}
 
 /**
  * Can use onclick="" in html but more common to see addeventlistener in real world application
@@ -36,30 +58,17 @@ inputBtn.addEventListener("click", function () {
 
     // Turn the myLeads string into an array
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads()
+    render(myLeads)
 
     // To verify that it works:
     console.log(localStorage.getItem("myLeads"))
 
 })
 
-function renderLeads() {
-    let listItems = ""
+deleteBtn.addEventListener("dblclick", function () {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
 
-    for (let i = 0; i < myLeads.length; i++) {
-        // Utilized Template strings to create more user friendly code
 
-        // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
-
-        listItems +=
-            `
-            <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
-                </a>
-            </li>
-        `
-    }
-    // Render the listItems inside the unordered list
-    ulEl.innerHTML = listItems
-}
